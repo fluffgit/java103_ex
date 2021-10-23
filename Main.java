4982ddd7743a1637304e7aacc4e7d5aca6892604
Marcin Lamek <fluffgit@gmail.com>
2021-10-23 14:34:37 +0200

    Upload3

--- Main.java
+++ Main.java
@@ -6,13 +6,14 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int b = 29*2;
-        int a = 29*3*6;
+        int b = 9;
+        int a = 12;
+//        System.out.println(dzielniki(a));
+//        System.out.println(dzielniki(b));
+//        System.out.println(NWD(dzielniki(a), dzielniki(b)));
 
-        System.out.println(dzielniki(a));
-        System.out.println(dzielniki(b));
-        System.out.println(NWD(dzielniki(a), dzielniki(b)));
-
+        System.out.println(divisor2(a, b));
+        System.out.println(leastCommonMultiple(a, b));
 
     }
 
@@ -22,19 +23,19 @@ public class Main {
         int returnDivisor = 0;
         boolean check = false;
         if (a.get(sizeA - 1) >= b.get(sizeB - 1)) {
-            for (int i = sizeA - 1; i >= 0; i--) {
-                for (int j = sizeB - 2; j >= 0; j--) {
+            for (int i = sizeA - 1; i != 0; i--) {
+                for (int j = sizeB - 1; j >= 0; j--) {
                     if (a.get(i).equals(b.get(j)) && !check) {
-                        returnDivisor= a.get(i);
+                        returnDivisor = a.get(i);
                         check = true;
                     }
                 }
             }
         } else {
-            for (int i = sizeB - 1; i >= 0; i--) {
-                for (int j = sizeA - 2; j >= 0; j--) {
+            for (int i = sizeB - 1; i != 0; i--) {
+                for (int j = sizeA - 1; j >= 0; j--) {
                     if (a.get(j).equals(b.get(i)) && !check) {
-                        returnDivisor= a.get(j);
+                        returnDivisor = a.get(j);
                         check = true;
                     }
                 }
@@ -45,11 +46,28 @@ public class Main {
 
     public static ArrayList<Integer> dzielniki(int i) {
         ArrayList<Integer> dzielnikiLista = new ArrayList<>();
-        for (int dzielnik = 1; dzielnik <= i; dzielnik++) {
+        for (int dzielnik = 1; dzielnik <= Math.abs(i); dzielnik++) {
             if (i % dzielnik == 0) {
                 dzielnikiLista.add(dzielnik);
             }
         }
         return dzielnikiLista;
     }
+
+
+    public static int divisor2(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+    public static int leastCommonMultiple(int a, int b) {
+        return a * b / divisor2(a, b);
+    }
+
 }
