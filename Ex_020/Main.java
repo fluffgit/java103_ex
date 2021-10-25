3d81fff93c0b6961d8b89831f2c541372ff3e2f0
Marcin Lamek <fluffgit@gmail.com>
2021-10-25 21:39:39 +0200

    ex20

--- Ex_020/Main.java
+++ Ex_020/Main.java
@@ -9,20 +9,29 @@ public class Main {
 
     public static void run(){
         int goal =generateRandom();
+        int min = 0;
+        int max =100;
         System.out.println("Podaj liczbę z zakresu [0,100]");
         System.out.println(goal);
         Scanner scanner = new Scanner(System.in);
         int choice=0;
+        int counter = 0;
         while(choice!=goal){
             choice =scanner.nextInt();
+            counter++;
             if(choice>goal){
-                System.out.println("za dużo");
+                max = choice;
+                System.out.println("za dużo, max: "+ max+", min "+min);
+
             }else if(choice<goal){
-                System.out.println("za mało");
+                min = choice;
+                System.out.println("za mało, max: "+ max+", min "+min);
+
             }else{
                 System.out.println("wygrałeś, szukana liczba to "+ goal);
             }
         }
+        System.out.println("Ilość prób: "+counter);
     }
     public static int generateRandom(){
         int max =100;
