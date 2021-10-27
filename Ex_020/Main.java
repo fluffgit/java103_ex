726d870b64eee8c28a69d0fb6ed0bbcd2d6b1313
Marcin Lamek <fluffgit@gmail.com>
2021-10-27 17:50:25 +0200

    final 34 b_2 :D

--- Ex_020/Main.java
+++ Ex_020/Main.java
@@ -1,37 +1,20 @@
 package w4_23102021.Ex_020;
 
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) {
-        if(isMature()) run();
-    }
 
-    public static void run() {
-        int goal = generateRandom();
-        int min = 0;
-        int max = 100;
-        System.out.println("Podaj liczbę z zakresu [0,100]");
-        System.out.println(goal);
-        Scanner scanner = new Scanner(System.in);
-        int choice = 0;
-        int counter = 0;
-        while (choice != goal && counter<4) {
-            choice = scanner.nextInt();
-            counter++;
-            if (choice > goal) {
-                max = choice;
-                System.out.println("za dużo, max: " + max + ", min " + min);
-            } else if (choice < goal) {
-                min = choice;
-                System.out.println("za mało, max: " + max + ", min " + min);
-            } else {
-                System.out.println("wygrałeś, szukana liczba to " + goal);
-            }
+    public static void main(String[] args) {
+        if (isMature()){
+            run();
+        }else{
+            System.out.println("nie jesteś pełnoletni");
         }
-        System.out.println("Ilość prób: " + counter +" przegrałes");
     }
 
     public static int generateRandom() {
@@ -40,13 +23,50 @@ public class Main {
         return (int) (Math.random() * (max - min) + min);
     }
 
+    public static void run(){
+        int goal = generateRandom();
+        Pattern number = Pattern.compile("^\\d+$");
+        Scanner scanner = new Scanner(System.in);
+        String choice = "";
+        int min = 0;
+        int max = 100;
+        boolean regexMatch = true;
+        boolean run = true;
+        System.out.println("szukamy "+goal);
+
+        while (run ) {
+            System.out.println("podaj liczbe do sprawdzenia");
+            System.out.println(min+" "+max);
+            choice = scanner.nextLine();
+            Matcher matcher = number.matcher(choice);
+            regexMatch = matcher.matches();
+
+            if (regexMatch) {
+                System.out.println("liczba");
+                if(Integer.parseInt(choice)>goal){
+                    max = Integer.parseInt(choice);
+                    System.out.println(min+" "+max);
+                }else if (Integer.parseInt(choice)<goal) {
+                    min = Integer.parseInt(choice);
+                    System.out.println(min + " " + max);
+                }else{
+                    Integer.parseInt(choice);
+                    System.out.println("wygrałes:" + choice);
+                    run = false;
+                }
+            } else if (!regexMatch)
+                System.out.println("bladedny znak");
+        }
+
+    }
+
     public static boolean isMature(){
+        System.out.println("Podaj datę urodzenia");
         LocalDate date1 = LocalDate.now();
         Scanner scanner = new Scanner(System.in);
         LocalDate date2 = LocalDate.parse(scanner.nextLine());
-        if(ChronoUnit.YEARS.between(date2, date1)>18){
-            return true;
-        }else return false;
+        return ChronoUnit.YEARS.between(date2, date1) > 18;
     }
+
 
 }
