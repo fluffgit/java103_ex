568bde077536c3acb88cbe6817370d6dd75604a3
Marcin Lamek <fluffgit@gmail.com>
2021-10-29 12:46:43 +0200

    rowery update

--- Rowery/Calculation.java
+++ Rowery/Calculation.java
@@ -58,30 +58,6 @@ public class Calculation {
         return uniqeBikes.size();
     }
 
-    public int test(ArrayList<Travel> travels){
-        for (Travel e:travels){
-
-            //dates.add(e.getStartTime222(e.getTravelStart(travels)));
-        }
-
-
-        return 0;
-    }
-
-
-
-    public ArrayList<Date> returnTime (ArrayList<Travel>travels){
-        ArrayList<Date> dates = new ArrayList<>();
-
-        for (Travel e:travels){
-
-            //dates.add(e.getStartTime222(e.getTravelStart(travels)));
-        }
-
-
-        return dates;
-    }
-
     public String findPopular2(ArrayList<Travel> travels) {
         //odchudzone
         //buduje Arraylist ze String-ów do sprawdzenia
@@ -112,6 +88,36 @@ public class Calculation {
                 mostRepeated = e;
         }
         return mostRepeated.getKey();
+    }
+
+    public String findPopular(ArrayList<Travel> travels) {
+        ArrayList<String> test = new ArrayList<>();
+        Iterator<Travel> iter = travels.iterator();
+        while (iter.hasNext()) {
+            test.add(iter.next().getBikeNumber());
+        }
+        Map<String, Integer> stringsCount = new HashMap<String, Integer>();
+        for (String string : test) {
+            if (string.length() > 0) {
+                string = string.toLowerCase();
+                Integer count = stringsCount.get(string);
+                if (count == null) count = new Integer(0);
+                count++;
+                stringsCount.put(string, count);
+            }
+        }
+        Map.Entry<String, Integer> mostRepeated = null;
+        for (Map.Entry<String, Integer> e : stringsCount.entrySet()) {
+            if (mostRepeated == null || mostRepeated.getValue() < e.getValue())
+                mostRepeated = e;
+        }
+        try {
+            return mostRepeated.getKey();
+        } catch (NullPointerException e) {
+            System.out.println("Cannot find most popular value at the List. Maybe all strings are empty");
+            return "";
+        }
+
     }
 
 
--- Rowery/Main.java
+++ Rowery/Main.java
@@ -71,35 +71,7 @@ public class Main {
 
     }
 
-    public static String findPopular(ArrayList<Travel> travels) {
-        ArrayList<String> test = new ArrayList<>();
-        Iterator<Travel> iter = travels.iterator();
-        while (iter.hasNext()) {
-            test.add(iter.next().getBikeNumber());
-        }
-        Map<String, Integer> stringsCount = new HashMap<String, Integer>();
-        for (String string : test) {
-            if (string.length() > 0) {
-                string = string.toLowerCase();
-                Integer count = stringsCount.get(string);
-                if (count == null) count = new Integer(0);
-                count++;
-                stringsCount.put(string, count);
-            }
-        }
-        Map.Entry<String, Integer> mostRepeated = null;
-        for (Map.Entry<String, Integer> e : stringsCount.entrySet()) {
-            if (mostRepeated == null || mostRepeated.getValue() < e.getValue())
-                mostRepeated = e;
-        }
-        try {
-            return mostRepeated.getKey();
-        } catch (NullPointerException e) {
-            System.out.println("Cannot find most popular value at the List. Maybe all strings are empty");
-            return "";
-        }
 
-    }
 
 
 
